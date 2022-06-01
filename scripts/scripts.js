52e9a083eaa5f992872f326e05fc4e127b95134d
unknown <prokop.n@gmail.com>
2022-06-01 23:05:51 +0200

    new photos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,9 @@ let isMobile = window.matchMedia("only screen and (max-width: 700px)").matches;
 if (isMobile) {
   map.src =
     "https://maps.google.com/maps?q=Hora%20Matky%20Bo%C5%BE%C3%AD%20Hede%C4%8D&t=&z=12&ie=UTF8&iwloc=&output=embed";
+} else {
+  map.src =
+    "https://maps.google.com/maps?q=Hora%20Matky%20Bo%C5%BE%C3%AD%20Hede%C4%8D&t=&z=14&ie=UTF8&iwloc=&output=embed";
 }
 
 acomodationRadios.forEach((radio) => {
